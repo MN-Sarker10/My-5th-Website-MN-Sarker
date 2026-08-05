cec20e9a136e1ed35b409aa2fe17ec698cb4db3e
MD Mostakin Sarker <100511153+MN-Sarker10@users.noreply.github.com>
2026-08-05 10:50:24 +0600

    File added Too

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function updateDateTime(){
+    const now = new Date();
+    const element = document.getElementById("time").innerText = now.toLocaleString();
+}
+
+setInterval(updateDateTime, 1000)
